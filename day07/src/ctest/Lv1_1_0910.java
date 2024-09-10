d832945b93972814c1a70514eb3c3cc9f88429d2
dmsqja <bum17822@naver.com>
2024-09-10 20:20:12 +0900

    day07 0910 ctest 코드

---
+++ day07/src/ctest/Lv1_1_0910.java
@@ -0,0 +1,19 @@
+package ctest;
+
+public class Lv1_1_0910 {
+    public static void main(String[] args) {
+
+    }
+    public static int solution(String dartResult) {
+        int answer = 0;
+        for (int i = 1; i < dartResult.length(); i++) {
+
+
+
+
+        }
+
+
+        return answer;
+    }
+}
