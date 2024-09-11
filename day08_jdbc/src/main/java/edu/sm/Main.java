11e3215230be602bfdc574939e20bc40c4d9671a
dmsqja <bum17822@naver.com>
2024-09-11 12:37:46 +0900

    연동

---
+++ day08_jdbc/src/main/java/edu/sm/Main.java
@@ -0,0 +1,14 @@
+package edu.sm;
+
+public class Main {
+    public static void main(String[] args) {
+        // 1. MySQL JDBC Driver Loading
+
+        // 2. Connection
+
+        // 3. SQL
+
+        // 4. Close
+
+    }
+}
