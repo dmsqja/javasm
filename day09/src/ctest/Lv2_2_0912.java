6411ce559c832befb3d650bc534b02eccbcc9755
eunbum <bum17822@naver.com>
2024-09-16 20:19:55 +0900

    0916 ctest 실습 코드

---
+++ day09/src/ctest/Lv2_2_0912.java
@@ -0,0 +1,12 @@
+package ctest;
+
+public class Lv2_2_0912 {
+    public int solution(int n, int a, int b)
+    {
+        int answer = 0;
+
+        System.out.println("Hello Java");
+
+        return answer;
+    }
+}
