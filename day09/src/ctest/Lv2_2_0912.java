5c4f1432d1f682677dcbc64e0ff5af9ac627e35b
eunbum <bum17822@naver.com>
2024-09-17 23:24:57 +0900

    0917 ctest 실습 코드

--- day09/src/ctest/Lv2_2_0912.java
+++ day09/src/ctest/Lv2_2_0912.java
@@ -1,11 +1,17 @@
 package ctest;
 
 public class Lv2_2_0912 {
-    public int solution(int n, int a, int b)
-    {
+    public static void main(String[] args) {
+        System.out.println(solution(8,4,7));
+    }
+    public static int solution(int n, int a, int b) {
         int answer = 0;
 
-        System.out.println("Hello Java");
+        while (a != b) {
+            a = (a + 1) / 2;
+            b = (b + 1) / 2;
+            answer++;
+        }
 
         return answer;
     }
