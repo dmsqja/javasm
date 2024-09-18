38a27eff9c6f4a4117ecba324f406a8c47f2d394
eunbum <bum17822@naver.com>
2024-09-18 16:52:28 +0900

    0918 ctest 실습 코드

---
+++ day09/src/ctest/Lv1_1_0918.java
@@ -0,0 +1,12 @@
+package ctest;
+
+import java.util.Scanner;
+
+public class Lv1_1_0918 {
+    public static void main(String[] args) {
+        String message = "Let's go!";
+
+        System.out.println("3\n2\n1");
+                System.out.println(message);
+    }
+}
