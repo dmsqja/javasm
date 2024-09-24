d48c0db9586fc2b90bcaab86c9009a65f283b58f
eunbum <bum17822@naver.com>
2024-09-24 20:14:17 +0900

    0924 ctest 실습 코드

---
+++ CodingTest/src/Lv2_1_0924.java
@@ -0,0 +1,19 @@
+public class Lv2_1_0924 {
+    public static void main(String[] args) {
+        System.out.println(solution(5));
+        System.out.println(solution(6));
+        System.out.println(solution(5000));
+    }
+    public static int solution (int n ){
+        int ans = 0;
+        while (n > 0){
+            if (n % 2 == 0){
+                n /= 2;
+            }else {
+                n--;
+                ans++;
+            }
+        }
+        return ans;
+    }
+}
